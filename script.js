f6ed12f7755d5829e3fab058f44bc534b7a07433
frsyi <fahmarosyi@gmail.com>
2025-08-30 12:19:07 +0700

    mencoba function hitung BMI

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+function hitungBMI(weight, height) {
+  const bmi = weight / (height / 100) ** 2;
+  let category = "";
 
+  if (bmi < 18.5) {
+    category = "Underweight";
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    category = "Normal Weight";
+  } else if (bmi >= 25 && bmi <= 29.9) {
+    category = "Overweight";
+  } else {
+    category = "Obesity";
+  }
+
+  return {
+    bmi: bmi.toFixed(2),
+    category: category,
+  };
+}
+
+console.log(hitungBMI(50, 160));
